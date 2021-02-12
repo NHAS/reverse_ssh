71899ad719a7b2b1593025beea8a6ecb85a02854
NHAS <jordanatararimu@gmail.com>
2021-02-12 16:57:38 +1300

    First attempt at background client fork

--- main.go
+++ main.go
@@ -12,10 +12,26 @@
 
 package main
 
-import "os"
+import (
+	"log"
+	"os"
+	"os/exec"
+	"time"
+)
 
 func main() {
 	if len(os.Args) > 1 {
+		log.Println("Am client, forking to background and disowning parent")
+
+		if len(os.Args) == 2 {
+			cmd := exec.Command(os.Args[0], "--client", "fork")
+			cmd.Start()
+
+			<-time.Tick(time.Second * 20)
+			log.Println("Ending parent")
+			return
+		}
+
 		client()
 		return
 	}
