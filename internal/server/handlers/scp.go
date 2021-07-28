77deab768dc6a0bead9eaec2ee3e41ae8a57ac08
NHAS <jordanatararimu@gmail.com>
2021-07-28 21:53:24 +1200

    Whoops, add missing files

---
+++ internal/server/handlers/scp.go
@@ -0,0 +1,51 @@
+package handlers
+
+import (
+	"fmt"
+	"io"
+	"strings"
+	"sync"
+
+	"github.com/NHAS/reverse_ssh/internal"
+	"golang.org/x/crypto/ssh"
+)
+
+func scp(connection ssh.Channel, requests <-chan *ssh.Request, mode string, path string, controllableClients *sync.Map) error {
+	go ssh.DiscardRequests(requests)
+
+	parts := strings.SplitN(path, ":", 2)
+
+	if len(parts) < 1 {
+		internal.ScpError("No target specified", connection)
+		return nil
+	}
+
+	conn, ok := controllableClients.Load(parts[0])
+	if !ok {
+		internal.ScpError(fmt.Sprintf("Invalid target, %s not found", parts[0]), connection)
+		return nil
+	}
+
+	device := conn.(ssh.Conn)
+
+	//This is not the standard spec, but I wanted to do it this way as its easier to deal with
+	scp, r, err := device.OpenChannel("scp", ssh.Marshal(&internal.Scp{Mode: mode, Path: parts[1]}))
+	if err != nil {
+		internal.ScpError("Could not connect to remote target", connection)
+		return err
+	}
+	go ssh.DiscardRequests(r)
+
+	go func() {
+		defer scp.Close()
+		defer connection.Close()
+
+		io.Copy(connection, scp)
+	}()
+
+	defer scp.Close()
+	defer connection.Close()
+	io.Copy(scp, connection)
+
+	return nil
+}
