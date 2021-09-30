0558b4eee42376396c7dbe7639ed887d6f754821
Chris Pritchard <chris@grislygrotto.nz>
2021-09-30 20:05:10 +1300

    revised arg names

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -13,8 +13,8 @@ func printHelp() {
 
 	fmt.Println("usage: ", filepath.Base(os.Args[0]), "[--key] [--publickeys] address")
 	fmt.Println("\t\taddress\tThe network address the server will listen on")
-	fmt.Println("\t\t--key\tPath to the ssh private key the server will use")
-	fmt.Println("\t\t--publickeys\tPath to the authorized_keys file or a given public key that control which users can talk to the server")
+	fmt.Println("\t\t--key\tPath to the ssh private key the server will use when talking with clients")
+	fmt.Println("\t\t--authorizedkeys\tPath to the authorized_keys file or a given public key that control which users can talk to the server")
 
 }
 
@@ -22,7 +22,7 @@ func main() {
 
 	privkey_path := flag.String("key", "", "Path to SSH private key, if omitted will generate a key on first use")
 	insecure := flag.Bool("insecure", false, "Ignore authorized_controllee_keys and allow any controllable client to connect")
-	authkey_path := flag.String("publickeys", "authorized_keys", "Path to authorized_keys file or a given public key, if omitted will look for an adjacent 'authorized_keys' file")
+	authkey_path := flag.String("authorizedkeys", "authorized_keys", "Path to authorized_keys file or a given public key, if omitted will look for an adjacent 'authorized_keys' file")
 
 	flag.Usage = printHelp
 
