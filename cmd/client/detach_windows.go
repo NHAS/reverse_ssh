abf1e860b896a5c8ee94572ea95afab8b43b1c89
NHAS <jordanatararimu@gmail.com>
2022-03-06 17:06:00 +1300

    Add release to windows

--- cmd/client/detach_windows.go
+++ cmd/client/detach_windows.go
@@ -25,5 +25,6 @@ func runOrFork(destination, fingerprint, proxyaddress string, fg, dt, rc bool) {
 
 	cmd := exec.Command(os.Args[0], append([]string{"--detach"}, os.Args[1:]...)...)
 	cmd.Start()
+	cmd.Process.Release()
 	log.Println("Ending parent")
 }
