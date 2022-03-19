5a194ce13841cf2a03e69eaeaffdba49349d6512
NHAS <jordanatararimu@gmail.com>
2022-03-20 02:03:33 +1300

    Remove ignoring singals

--- cmd/client/detach_windows.go
+++ cmd/client/detach_windows.go
@@ -19,21 +19,7 @@ func runOrFork(destination, fingerprint, proxyaddress string, fg, dt, rc bool) {
 			procAttachConsole := modkernel32.NewProc("FreeConsole")
 			syscall.Syscall(procAttachConsole.Addr(), 0, 0, 0, 0)
 		}
-		// Set up channel on which to send signal notifications.
-		// We must use a buffered channel or risk missing the signal
-		// if we're not ready to receive when the signal is sent.
-		c := make(chan os.Signal, 1)
 
-		// Passing no signals to Notify means that
-		// all signals will be sent to the channel.
-		signal.Notify(c)
-
-		// Block until any signal is received.
-		go func() {
-			for range c {
-				// Ignore all signals. Because yes
-			}
-		}()
 		client.Run(destination, fingerprint, proxyaddress, rc)
 		return
 	}
