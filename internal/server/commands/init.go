6c47514eacc86a06f9042a7e481bdd605eb16bf0
NHAS <jordanatararimu@gmail.com>
2022-03-20 19:59:43 +1300

    Add who command to list current server users

--- internal/server/commands/init.go
+++ internal/server/commands/init.go
@@ -9,25 +9,27 @@ import (
 //This is used for help, so we can generate the nice table
 // I would prefer if we could do some sort of autoregistration process for these
 var allCommands = map[string]terminal.Command{
-	"ls":      &List{},
+	"ls":      &list{},
 	"help":    &help{},
 	"kill":    &kill{},
 	"connect": &connect{},
 	"exit":    &exit{},
 	"link":    &link{},
 	"exec":    &exec{},
+	"who":     &who{},
 }
 
 func CreateCommands(user *internal.User, log logger.Logger) map[string]terminal.Command {
 
 	var o = map[string]terminal.Command{
-		"ls":      &List{},
-		"help":    Help(),
+		"ls":      &list{},
+		"help":    &help{},
 		"kill":    Kill(log),
 		"connect": Connect(user, log),
 		"exit":    &exit{},
 		"link":    &link{},
 		"exec":    &exec{},
+		"who":     &who{},
 	}
 
 	return o
--- internal/users.go
+++ internal/users.go
@@ -2,11 +2,17 @@ package internal
 
 import (
 	"errors"
+	"fmt"
+	"sort"
+	"sync"
 
 	"golang.org/x/crypto/ssh"
 )
 
 var ErrNilServerConnection = errors.New("The server connection was nil for the client")
+
+var lUsers sync.RWMutex
+var users map[string]bool = make(map[string]bool)
 
 type User struct {
 	// This is the users connection to the server itself, creates new channels and whatnot. NOT to get io.Copy'd
@@ -18,6 +24,9 @@ type User struct {
 
 	// Remote forwards sent by user
 	SupportedRemoteForwards map[RemoteForwardRequest]bool //(set)
+
+	// So we can capture details about who is currently using the rssh server
+	ConnectionDetails string
 }
 
 func CreateUser(ServerConnection ssh.Conn) (us *User, err error) {
@@ -29,13 +38,36 @@ func CreateUser(ServerConnection ssh.Conn) (us *User, err error) {
 	us = &User{
 		ServerConnection:        ServerConnection,
 		SupportedRemoteForwards: make(map[RemoteForwardRequest]bool),
+		ConnectionDetails:       fmt.Sprintf("%s@%s", ServerConnection.User(), ServerConnection.RemoteAddr().String()),
 	}
 
+	lUsers.Lock()
+	defer lUsers.Unlock()
+
+	users[us.ConnectionDetails] = true
+
+	return
+}
+
+func ListUsers() (userList []string) {
+	lUsers.RLock()
+	defer lUsers.RUnlock()
+
+	for user := range users {
+		userList = append(userList, user)
+	}
+
+	sort.Strings(userList)
 	return
 }
 
 func DeleteUser(us *User) {
 	if us != nil {
+
+		lUsers.Lock()
+		delete(users, us.ConnectionDetails)
+		lUsers.Unlock()
+
 		if us.ServerConnection != nil {
 			defer us.ServerConnection.Close()
 		}
