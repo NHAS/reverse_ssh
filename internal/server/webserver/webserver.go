66159a15c0bcd21e82f384b34fea7e3b18adaeee
Josh H <joshuahawking1@gmail.com>
2022-07-09 15:30:16 +0100

    Update webserver.go

--- internal/server/webserver/webserver.go
+++ internal/server/webserver/webserver.go
@@ -33,8 +33,8 @@ func Start(webListener net.Listener, connectBackAddress, projRoot string, public
 	}
 
 	srv := &http.Server{
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 5 * time.Second,
+		ReadTimeout:  20 * time.Second,
+		WriteTimeout: 20 * time.Second,
 		Handler:      buildAndServe(projRoot, connectBackAddress, validPlatforms, validArchs),
 	}
 
