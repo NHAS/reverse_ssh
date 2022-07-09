4e0791f4bb92765e8c67263fca323386d0821ec0
Josh H <joshuahawking1@gmail.com>
2022-07-09 13:58:07 +0100

    Change timeouts on webserver to 5 seconds

--- internal/server/webserver/webserver.go
+++ internal/server/webserver/webserver.go
@@ -33,8 +33,8 @@ func Start(webListener net.Listener, connectBackAddress, projRoot string, public
 	}
 
 	srv := &http.Server{
-		ReadTimeout:  2 * time.Second,
-		WriteTimeout: 2 * time.Second,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
 		Handler:      buildAndServe(projRoot, connectBackAddress, validPlatforms, validArchs),
 	}
 
