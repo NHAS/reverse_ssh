13993568d0f64cef4fbda9aa85fcd8b67905673e
NHAS <jordanatararimu@gmail.com>
2022-07-17 21:09:42 +1200

    Implement icmp

--- go.mod
+++ go.mod
@@ -5,16 +5,20 @@ go 1.17
 require (
 	github.com/ActiveState/termtest/conpty v0.5.0
 	github.com/creack/pty v1.1.17
+	github.com/nicocha30/gvisor-ligolo v0.0.0-20220711100921-110ade85d411
 	github.com/pkg/sftp v1.13.4
 	golang.org/x/crypto v0.0.0-20220214200702-86341886e292
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a
-	gvisor.dev/gvisor v0.0.0-20220715034956-0772aef20da7
 )
 
 require (
 	github.com/Azure/go-ansiterm v0.0.0-20170929234023-d6e3b3328b78 // indirect
+	github.com/go-ping/ping v1.1.0 // indirect
 	github.com/google/btree v1.0.1 // indirect
+	github.com/google/uuid v1.2.0 // indirect
 	github.com/kr/fs v0.1.0 // indirect
 	github.com/stretchr/testify v1.7.1 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/time v0.0.0-20191024005414-555d28b269f0 // indirect
 )
--- internal/client/handlers/tun.go
+++ internal/client/handlers/tun.go
@@ -1,26 +1,34 @@
 package handlers
 
 import (
+	"bytes"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"net"
+	"os/exec"
+	"runtime"
+	"sync"
+	"time"
 
 	"github.com/NHAS/reverse_ssh/internal"
 	"github.com/NHAS/reverse_ssh/pkg/logger"
+	"github.com/go-ping/ping"
+	"github.com/nicocha30/gvisor-ligolo/pkg/buffer"
+	"github.com/nicocha30/gvisor-ligolo/pkg/tcpip"
+	"github.com/nicocha30/gvisor-ligolo/pkg/tcpip/adapters/gonet"
+	"github.com/nicocha30/gvisor-ligolo/pkg/tcpip/header"
+	"github.com/nicocha30/gvisor-ligolo/pkg/tcpip/network/ipv4"
+	"github.com/nicocha30/gvisor-ligolo/pkg/tcpip/network/ipv6"
+	"github.com/nicocha30/gvisor-ligolo/pkg/tcpip/stack"
+	"github.com/nicocha30/gvisor-ligolo/pkg/tcpip/transport/icmp"
+	"github.com/nicocha30/gvisor-ligolo/pkg/tcpip/transport/raw"
+	"github.com/nicocha30/gvisor-ligolo/pkg/tcpip/transport/tcp"
+	"github.com/nicocha30/gvisor-ligolo/pkg/tcpip/transport/udp"
+	"github.com/nicocha30/gvisor-ligolo/pkg/waiter"
 	"golang.org/x/crypto/ssh"
-	"gvisor.dev/gvisor/pkg/buffer"
-	"gvisor.dev/gvisor/pkg/tcpip"
-	"gvisor.dev/gvisor/pkg/tcpip/adapters/gonet"
-	"gvisor.dev/gvisor/pkg/tcpip/header"
-	"gvisor.dev/gvisor/pkg/tcpip/network/ipv4"
-	"gvisor.dev/gvisor/pkg/tcpip/network/ipv6"
-	"gvisor.dev/gvisor/pkg/tcpip/stack"
-	"gvisor.dev/gvisor/pkg/tcpip/transport/icmp"
-	"gvisor.dev/gvisor/pkg/tcpip/transport/tcp"
-	"gvisor.dev/gvisor/pkg/tcpip/transport/udp"
-	"gvisor.dev/gvisor/pkg/waiter"
 )
 
 func Tun(_ *internal.User, newChannel ssh.NewChannel, l logger.Logger) {
@@ -64,18 +72,22 @@ func Tun(_ *internal.User, newChannel ssh.NewChannel, l logger.Logger) {
 		},
 		HandleLocal: false,
 	})
+	defer ns.Close()
 
 	linkEP := NewSSHEndpoint(tunnel)
 
+	const NICID = 1
 	// Create a new NIC
-	if err := ns.CreateNIC(1, linkEP); err != nil {
-		log.Printf("CreateNIC: %v", err)
+	if err := ns.CreateNIC(NICID, linkEP); err != nil {
+		l.Error("CreateNIC: %v", err)
 		return
 	}
 
-	// Gvisor Hack: Disable ICMP handling.
-	ns.SetICMPLimit(0)
-	ns.SetICMPBurst(0)
+	err = icmpResponder(ns)
+	if err != nil {
+		l.Error("Unable to create icmp responder: %v", err)
+		return
+	}
 
 	// Forward TCP connections
 	tcpHandler := tcp.NewForwarder(ns, 30000, 4000, forwardTCP)
@@ -91,17 +103,17 @@ func Tun(_ *internal.User, newChannel ssh.NewChannel, l logger.Logger) {
 	ns.SetRouteTable([]tcpip.Route{
 		{
 			Destination: header.IPv4EmptySubnet,
-			NIC:         1,
+			NIC:         NICID,
 		},
 		{
 			Destination: header.IPv6EmptySubnet,
-			NIC:         1,
+			NIC:         NICID,
 		},
 	})
 
-	// Enable forwarding
-	ns.SetForwardingDefaultAndAllNICs(ipv4.ProtocolNumber, true)
-	ns.SetForwardingDefaultAndAllNICs(ipv6.ProtocolNumber, true)
+	// Disable forwarding
+	ns.SetForwardingDefaultAndAllNICs(ipv4.ProtocolNumber, false)
+	ns.SetForwardingDefaultAndAllNICs(ipv6.ProtocolNumber, false)
 
 	// Enable TCP SACK
 	nsacks := tcpip.TCPSACKEnabled(true)
@@ -112,8 +124,8 @@ func Tun(_ *internal.User, newChannel ssh.NewChannel, l logger.Logger) {
 	ns.SetTransportProtocolOption(tcp.ProtocolNumber, &synCookies)
 
 	// Allow packets from all sources/destinations
-	ns.SetPromiscuousMode(1, true)
-	ns.SetSpoofing(1, true)
+	ns.SetPromiscuousMode(NICID, true)
+	ns.SetSpoofing(NICID, true)
 
 	ssh.DiscardRequests(req)
 
@@ -123,75 +135,95 @@ func Tun(_ *internal.User, newChannel ssh.NewChannel, l logger.Logger) {
 
 func forwardUDP(stack *stack.Stack) func(request *udp.ForwarderRequest) {
 	return func(request *udp.ForwarderRequest) {
+		go func() {
+			id := request.ID()
 
-		id := request.ID()
+			var wq waiter.Queue
 
-		var wq waiter.Queue
+			ep, iperr := request.CreateEndpoint(&wq)
+			if iperr != nil {
+				return
+			}
 
-		ep, iperr := request.CreateEndpoint(&wq)
-		if iperr != nil {
-			ep.Close()
-			return
-		}
-		defer ep.Close()
+			log.Printf("tun [+] %s -> %s:%d/udp\n", id.RemoteAddress, id.LocalAddress, id.LocalPort)
 
-		log.Printf("[+] %s -> %s:%d/udp\n", id.RemoteAddress, id.LocalAddress, id.LocalPort)
+			fwdDst := net.UDPAddr{
+				IP:   net.ParseIP(id.LocalAddress.String()),
+				Port: int(id.LocalPort),
+			}
 
-		fwdDst := net.UDPAddr{
-			IP:   net.ParseIP(id.LocalAddress.String()),
-			Port: int(id.LocalPort),
-		}
+			remote, err := net.DialUDP("udp", nil, &fwdDst)
+			if err != nil {
+				return
+			}
 
-		conn, err := net.Dial("udp", fwdDst.String())
-		if err != nil {
-			return
-		}
-		defer conn.Close()
+			local := gonet.NewUDPConn(stack, &wq, ep)
 
-		gonetConn := gonet.NewUDPConn(stack, &wq, ep)
-		defer gonetConn.Close()
-
-		go io.Copy(gonetConn, conn)
-		_, err = io.Copy(conn, gonetConn)
-
+			err = Proxy(local, remote)
+			if err != nil {
+				log.Printf("proxy connection closed with error: %v", err)
+			}
+		}()
 	}
 }
 
 func forwardTCP(request *tcp.ForwarderRequest) {
-	id := request.ID()
+	go func() {
+		id := request.ID()
 
-	var wq waiter.Queue
-	ep, errTcp := request.CreateEndpoint(&wq)
-	if errTcp != nil {
-		fmt.Printf("r.CreateEndpoint() = %v\n", errTcp)
-		request.Complete(true)
-		return
-	}
+		var wq waiter.Queue
+		ep, errTcp := request.CreateEndpoint(&wq)
+		if errTcp != nil {
+			fmt.Printf("r.CreateEndpoint() = %v\n", errTcp)
+			request.Complete(true)
+			return
+		}
 
-	request.Complete(false)
+		fwdDst := net.TCPAddr{
+			IP:   net.ParseIP(id.LocalAddress.String()),
+			Port: int(id.LocalPort),
+		}
 
-	c := gonet.NewTCPConn(&wq, ep)
-	defer c.Close()
+		log.Printf("[+] %s -> %s:%d/tcp\n", id.RemoteAddress, id.LocalAddress, id.LocalPort)
 
-	fwdDst := net.TCPAddr{
-		IP:   net.ParseIP(id.LocalAddress.String()),
-		Port: int(id.LocalPort),
-	}
+		local := gonet.NewTCPConn(&wq, ep)
 
-	log.Printf("[+] %s -> %s:%d/tcp\n", id.RemoteAddress, id.LocalAddress, id.LocalPort)
+		remote, err := net.Dial("tcp", fwdDst.String())
+		if err != nil {
+			fmt.Println(err)
+			request.Complete(true)
+			return
+		}
 
-	remote, err := net.Dial("tcp", fwdDst.String())
+		err = Proxy(local, remote)
+		if err != nil {
+			log.Printf("proxy connection closed with error: %v", err)
+		}
+	}()
+}
+
+func Proxy(c1, c2 net.Conn) error {
+	connClosed := make(chan error, 2)
+
+	defer c1.Close()
+	defer c2.Close()
+
+	go func() {
+		_, err := io.Copy(c1, c2)
+		connClosed <- err
+	}()
+
+	go func() {
+		_, err := io.Copy(c2, c1)
+		connClosed <- err
+	}()
+
+	err := <-connClosed
 	if err != nil {
-		c.Close()
-		request.Complete(true)
-		fmt.Println(err)
-		return
+		return err
 	}
-	defer remote.Close()
-	defer c.Close()
 
-	go io.Copy(remote, c)
-	_, err = io.Copy(c, remote)
+	return nil
 }
 
 type SSHEndpoint struct {
@@ -233,11 +265,15 @@ func (m *SSHEndpoint) Attach(dispatcher stack.NetworkDispatcher) {
 
 func (m *SSHEndpoint) dispatchLoop() {
 	for {
-		packet := make([]byte, 1500)
+		packet := make([]byte, 1504)
 
 		n, err := m.tunnel.Read(packet)
 		if err != nil {
 			break
+		}
+
+		if len(packet) < 4 {
+			continue
 		}
 
 		//Remove the SSH added family address uint32 (for layer 3 tun)
@@ -277,19 +313,24 @@ func (m *SSHEndpoint) WritePackets(pkts stack.PacketBufferList) (int, tcpip.Erro
 	return n, nil
 }
 
+var lock sync.Mutex
+
 // WritePacket writes outbound packets
 func (m *SSHEndpoint) WritePacket(pkt *stack.PacketBuffer) tcpip.Error {
-	var buf buffer.Buffer
+
 	pktBuf := pkt.Buffer()
-	buf.Merge(&pktBuf)
+
+	//I have quite literally no idea why a lock here fixes ssh issues
+	lock.Lock()
+	defer lock.Unlock()
 
 	// 3.2 Frame Format
 	// https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/Documentation/networking/tuntap.rst?id=HEAD
-	packet := make([]byte, 4)
+	packet := make([]byte, 4+pktBuf.Size())
 	binary.BigEndian.PutUint16(packet, 1)
 	binary.BigEndian.PutUint16(packet[2:], uint16(pkt.NetworkProtocolNumber))
 
-	packet = append(packet, buf.Flatten()...)
+	copy(packet[4:], pktBuf.Flatten())
 
 	if _, err := m.tunnel.Write(packet); err != nil {
 		return &tcpip.ErrInvalidEndpointState{}
@@ -312,4 +353,198 @@ func (*SSHEndpoint) AddHeader(pkt *stack.PacketBuffer) {
 // WriteRawPacket implements stack.LinkEndpoint.
 func (*SSHEndpoint) WriteRawPacket(*stack.PacketBuffer) tcpip.Error {
 	return &tcpip.ErrNotSupported{}
+}
+
+func icmpResponder(s *stack.Stack) error {
+
+	var wq waiter.Queue
+	rawProto, rawerr := raw.NewEndpoint(s, ipv4.ProtocolNumber, icmp.ProtocolNumber4, &wq)
+	if rawerr != nil {
+		return errors.New("could not create raw endpoint")
+	}
+	if err := rawProto.Bind(tcpip.FullAddress{}); err != nil {
+		return errors.New("could not bind raw endpoint")
+	}
+	go func() {
+		we, ch := waiter.NewChannelEntry(waiter.ReadableEvents)
+		wq.EventRegister(&we)
+		for {
+			var buff bytes.Buffer
+			_, err := rawProto.Read(&buff, tcpip.ReadOptions{})
+
+			if _, ok := err.(*tcpip.ErrWouldBlock); ok {
+				// Wait for data to become available.
+				select {
+				case <-ch:
+					_, err := rawProto.Read(&buff, tcpip.ReadOptions{})
+
+					if err != nil {
+						if _, ok := err.(*tcpip.ErrWouldBlock); ok {
+							// Oh, a race condition?
+							continue
+						} else {
+							// This is bad.
+							panic(err)
+						}
+					}
+
+					iph := header.IPv4(buff.Bytes())
+
+					hlen := int(iph.HeaderLength())
+					if buff.Len() < hlen {
+						return
+					}
+
+					// Reconstruct a ICMP PacketBuffer from bytes.
+					view := buffer.NewWithData(buff.Bytes())
+					packetbuff := stack.NewPacketBuffer(stack.PacketBufferOptions{
+						Payload:            view,
+						ReserveHeaderBytes: hlen,
+					})
+
+					packetbuff.NetworkProtocolNumber = ipv4.ProtocolNumber
+					packetbuff.TransportProtocolNumber = icmp.ProtocolNumber4
+					packetbuff.NetworkHeader().Consume(hlen)
+
+					go func() {
+						if TryResolve(iph.DestinationAddress().String()) {
+							ProcessICMP(s, packetbuff)
+						}
+					}()
+
+				}
+			}
+
+		}
+	}()
+	return nil
+}
+
+// ProcessICMP send back a ICMP echo reply from after receiving a echo request.
+// This code come mostly from pkg/tcpip/network/ipv4/icmp.go
+func ProcessICMP(nstack *stack.Stack, pkt *stack.PacketBuffer) {
+	// (gvisor) pkg/tcpip/network/ipv4/icmp.go:174 - handleICMP
+
+	// ICMP packets don't have their TransportHeader fields set. See
+	// icmp/protocol.go:protocol.Parse for a full explanation.
+	v, ok := pkt.Data().PullUp(header.ICMPv4MinimumSize)
+	if !ok {
+		return
+	}
+	h := header.ICMPv4(v)
+
+	// Only do in-stack processing if the checksum is correct.
+	if pkt.Data().AsRange().Checksum() != 0xffff {
+		return
+	}
+
+	iph := header.IPv4(pkt.NetworkHeader().View())
+	var newOptions header.IPv4Options
+
+	// TODO(b/112892170): Meaningfully handle all ICMP types.
+	switch h.Type() {
+	case header.ICMPv4Echo:
+
+		replyData := pkt.Data().AsRange().ToOwnedView()
+		ipHdr := header.IPv4(pkt.NetworkHeader().View())
+
+		localAddressBroadcast := pkt.NetworkPacketInfo.LocalAddressBroadcast
+
+		// It's possible that a raw socket expects to receive this.
+		pkt = nil
+
+		// Take the base of the incoming request IP header but replace the options.
+		replyHeaderLength := uint8(header.IPv4MinimumSize + len(newOptions))
+		replyIPHdr := header.IPv4(append(iph[:header.IPv4MinimumSize:header.IPv4MinimumSize], newOptions...))
+		replyIPHdr.SetHeaderLength(replyHeaderLength)
+
+		// As per RFC 1122 section 3.2.1.3, when a host sends any datagram, the IP
+		// source address MUST be one of its own IP addresses (but not a broadcast
+		// or multicast address).
+		localAddr := ipHdr.DestinationAddress()
+		if localAddressBroadcast || header.IsV4MulticastAddress(localAddr) {
+			localAddr = ""
+		}
+
+		r, err := nstack.FindRoute(1, localAddr, ipHdr.SourceAddress(), ipv4.ProtocolNumber, false /* multicastLoop */)
+		if err != nil {
+			// If we cannot find a route to the destination, silently drop the packet.
+			return
+		}
+		defer r.Release()
+
+		replyIPHdr.SetSourceAddress(r.LocalAddress())
+		replyIPHdr.SetDestinationAddress(r.RemoteAddress())
+		replyIPHdr.SetTTL(r.DefaultTTL())
+
+		replyICMPHdr := header.ICMPv4(replyData)
+		replyICMPHdr.SetType(header.ICMPv4EchoReply)
+		replyICMPHdr.SetChecksum(0)
+		replyICMPHdr.SetChecksum(^header.Checksum(replyData, 0))
+
+		replyBuf := buffer.NewWithData(replyIPHdr)
+		replyBuf.AppendOwned(replyData)
+		replyPkt := stack.NewPacketBuffer(stack.PacketBufferOptions{
+			ReserveHeaderBytes: int(r.MaxHeaderLength()),
+			Payload:            replyBuf,
+		})
+
+		replyPkt.TransportProtocolNumber = header.ICMPv4ProtocolNumber
+
+		if err := r.WriteHeaderIncludedPacket(replyPkt); err != nil {
+			return
+		}
+	}
+}
+
+// TryResolve tries to discover if the remote host is up using ICMP
+func TryResolve(address string) bool {
+	methods := []func(string) (bool, error){
+		RawPinger,
+		CommandPinger,
+	}
+	for _, method := range methods {
+		if result, err := method(address); err == nil {
+			return result
+		}
+	}
+	// Everything failed...
+	return false
+}
+
+// RawPinger use ICMP sockets to discover if a host is up. This could require administrative permissions on some hosts
+func RawPinger(target string) (bool, error) {
+	pinger, err := ping.NewPinger(target)
+	if err != nil {
+		return false, err
+	}
+	pinger.Count = 1
+	pinger.Timeout = 4 * time.Second // NMAP default timeout ?
+	if runtime.GOOS == "windows" {
+		pinger.SetPrivileged(true)
+	}
+	err = pinger.Run()
+	if err != nil {
+		return false, err
+	}
+
+	return pinger.PacketsRecv != 0, nil
+}
+
+// CommandPinger uses the internal ping command (dirty), but should not require privileges
+func CommandPinger(target string) (bool, error) {
+	countArg := "-c"
+	waitArg := "-W"
+	waitTime := "3"
+	if runtime.GOOS == "windows" {
+		countArg = "/n"
+		waitArg = "/w"
+		waitTime = "3000"
+	}
+
+	cmd := exec.Command("ping", countArg, "1", waitArg, waitTime, target)
+	if err := cmd.Run(); err != nil {
+		return false, err
+	}
+	return true, nil
 }
